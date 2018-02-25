e4e52f52fe0bfb7fde62843f1a27591d97f41c05
andro2master <andro2master@gmail.com>
2018-02-25 12:04:33 +0530

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,16 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Rotate */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+
+})();
